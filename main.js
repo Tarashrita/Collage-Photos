5ce83b8e6c8ab1906effb6d7f357719337449d97
Tarashrita <106442772+Tarashrita@users.noreply.github.com>
2023-02-11 23:13:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,3 +6,26 @@ document.getElementById("textbox").innerHTML="";
 recognition.Start();
 
 }
+recognition.onresult = function(event){
+    console.log(event);
+    var Content = event.results[0][0].transcript;
+    document.getElementById("textbox").innerHTML = Content;
+    console.log(Content);
+    speak();
+    }
+    
+    function speak(){
+        var synth = window.speechSynthesis;
+       var speakData = document.getElementById("textbox").value;
+       var utterThis = new SpeechSynthesisUtterance(speakData);
+       synth.speak(utterThis);
+    }
+    Webcam.set({
+        width : 360,
+       height : 250,
+       image_format : "png",
+       png_quality : 90
+    });
+    camera = document.getElementById("webcam");
+    Webcam.attach(camera);
+    
