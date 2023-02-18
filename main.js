58faf51ce2c91ee40dffba21193d0de22d2cc10c
Tarashrita <106442772+Tarashrita@users.noreply.github.com>
2023-02-18 21:02:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,7 +11,11 @@ recognition.onresult = function(event){
     var Content = event.results[0][0].transcript;
     document.getElementById("textbox").innerHTML = Content;
     console.log(Content);
-    speak();
+    if(Content=="take my selfie"){
+        console.log("taking selfie")
+        speak();
+    }
+   
     }
     
     function speak(){
@@ -28,4 +32,16 @@ recognition.onresult = function(event){
     });
     camera = document.getElementById("webcam");
     Webcam.attach(camera);
-    
+    function takeSnapshot() {
+    Webcam.snap(function(data_uri){
+        if(img_id=="selfie1"){document.getElementById("result1").innerHTML = '<img id="selfie_image1" src="'+data_uri+'">';}
+        if(img_id=="selfie2"){document.getElementById("result2").innerHTML = '<img id="selfie_image2" src="'+data_uri+'">';}
+        if(img_id=="selfie3"){document.getElementById("result3").innerHTML = '<img id="selfie_image3" src="'+data_uri+'">';}
+    })
+}
+    function Save() {
+        link = document.getElementById("link");
+        image = document.getElementById("selfie_image").src;
+        link.href=image;
+        link.click();
+    }
